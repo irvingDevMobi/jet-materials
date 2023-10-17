d644b5bd7fafbedd44ce23a861269c1b8766c16b
Irving Lop <irving.dev.mobi@gmail.com>
2023-10-17 16:21:22 -0600

    C2 - Progress Indicators

--- 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ProgressIndicatorsScreen.kt
+++ 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ProgressIndicatorsScreen.kt
@@ -37,9 +37,14 @@ package com.yourcompany.android.jetpackcompose.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
@@ -47,15 +52,19 @@ import com.yourcompany.android.jetpackcompose.router.Screen
 @Composable
 fun ProgressIndicatorScreen() {
 
-  Column(
-      modifier = Modifier.fillMaxSize(),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center
-  ) {
-    //TODO add your code here
-  }
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        CircularProgressIndicator(
+            color = colorResource(id = R.color.colorPrimary),
+            strokeWidth = 5.dp
+        )
+        LinearProgressIndicator(progress = 0.5f)
+    }
 
-  BackButtonHandler {
-    JetFundamentalsRouter.navigateTo(Screen.Navigation)
-  }
+    BackButtonHandler {
+        JetFundamentalsRouter.navigateTo(Screen.Navigation)
+    }
 }
