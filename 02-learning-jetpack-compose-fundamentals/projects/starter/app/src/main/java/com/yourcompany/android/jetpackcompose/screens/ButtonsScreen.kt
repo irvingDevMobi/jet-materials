8fb8b5e123bb14f60fa12bbe44dcc0abd2df900f
Irving Lop <irving.dev.mobi@gmail.com>
2023-10-17 16:10:32 -0600

    C2 - Buttons

--- 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ButtonsScreen.kt
+++ 02-learning-jetpack-compose-fundamentals/projects/starter/app/src/main/java/com/yourcompany/android/jetpackcompose/screens/ButtonsScreen.kt
@@ -34,43 +34,96 @@
 
 package com.yourcompany.android.jetpackcompose.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.RadioButton
+import androidx.compose.material.RadioButtonDefaults
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.yourcompany.android.jetpackcompose.R
 import com.yourcompany.android.jetpackcompose.router.BackButtonHandler
 import com.yourcompany.android.jetpackcompose.router.JetFundamentalsRouter
 import com.yourcompany.android.jetpackcompose.router.Screen
 
 @Composable
 fun ExploreButtonsScreen() {
-  Column(modifier = Modifier.fillMaxSize(),
-      horizontalAlignment = Alignment.CenterHorizontally,
-      verticalArrangement = Arrangement.Center) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
 
-    MyButton()
-    MyRadioGroup()
-    MyFloatingActionButton()
+        MyButton()
+        MyRadioGroup()
+        MyFloatingActionButton()
 
-    BackButtonHandler {
-      JetFundamentalsRouter.navigateTo(Screen.Navigation)
+        BackButtonHandler {
+            JetFundamentalsRouter.navigateTo(Screen.Navigation)
+        }
     }
-  }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun MyButton() {
-  //TODO add your code here
+    Button(
+        onClick = { },
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = colorResource(id = R.color.colorPrimary),
+        ),
+        border = BorderStroke(1.dp, color = colorResource(id = R.color.colorPrimaryDark)),
+    ) {
+        Text(text = stringResource(id = R.string.button_text), color = Color.White)
+    }
 }
 
+@Preview(showBackground = true)
 @Composable
 fun MyRadioGroup() {
-  //TODO add your code here
+    val radioButtons = listOf(0, 1, 2)
+    val selectedButton = remember { mutableStateOf(radioButtons.first()) }
+    Column {
+        radioButtons.forEach { index ->
+            val isSelected = index == selectedButton.value
+            val colors = RadioButtonDefaults.colors(
+                selectedColor = colorResource(id = R.color.colorPrimary),
+                unselectedColor = colorResource(id = R.color.colorPrimaryDark),
+                disabledColor = Color.LightGray
+            )
+            RadioButton(
+                selected = isSelected,
+                onClick = { selectedButton.value = index },
+                colors = colors
+            )
+        }
+    }
 }
 
 @Composable
+@Preview(showBackground = true)
 fun MyFloatingActionButton() {
-  //TODO add your code here
+    FloatingActionButton(
+        onClick = { },
+        backgroundColor = colorResource(id = R.color.colorPrimary),
+        contentColor = Color.White
+    ) {
+        Icon(Icons.Filled.Favorite, contentDescription = "Test FAB")
+    }
 }
